2982fae4991418f1bbc3129352784b2d785d2fe4
Danyil <88362657+ImCapture@users.noreply.github.com>
2023-05-15 14:27:36 +0300

    renamed employee_hierarchyBS.html; created basic employee_list.html

--- ancTask/employees/urls.py
+++ ancTask/employees/urls.py
@@ -5,7 +5,8 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='employees/sheets/employee_hierarchyBS.html'), name='employee_hierarchy'),
+    path('', TemplateView.as_view(template_name='employees/sheets/employee_hierarchy.html'), name='employee_hierarchy'),
     path('employee_hierarchy_json/', EmployeeHierarchyJson.as_view(), name='employee_hierarchy_json'),
     path('employee_list_json/', EmployeeListJson.as_view(), name='employee_list_json'),
+    path('list/', TemplateView.as_view(template_name='employees/sheets/employee_list.html'), name='employee_list'),
 ]
