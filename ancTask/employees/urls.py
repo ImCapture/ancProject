501a9e6e8486c77f068ca62f35afa290371cb396
Danyil <88362657+ImCapture@users.noreply.github.com>
2023-05-15 12:31:07 +0300

    created and urled EmployeeListJson view

--- ancTask/employees/urls.py
+++ ancTask/employees/urls.py
@@ -1,4 +1,5 @@
 from .views import EmployeeHierarchyJson
+from .views import EmployeeListJson
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -6,4 +7,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='employees/sheets/employee_hierarchyBS.html'), name='employee_hierarchy'),
     path('employee_hierarchy_json/', EmployeeHierarchyJson.as_view(), name='employee_hierarchy_json'),
+    path('employee_list_json/', EmployeeListJson.as_view(), name='employee_list_json'),
 ]
--- ancTask/employees/views.py
+++ ancTask/employees/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .models import Employee
 from django.http import JsonResponse
 from django.views import View
+import json
 
 
 class EmployeeHierarchyJson(View):
@@ -21,7 +22,26 @@ class EmployeeHierarchyJson(View):
                 # "children": list(employee.children.values('id'))
 
                 # Меняем parent_id root работника с Null на # по требованию AJAX JStree
-                "parent": employee.parent_id or '#'
+                "parent": employee.parent_id or '#',
+            } for employee in employees
+        ]
+        return JsonResponse(employees_json, safe=False)
+
+class EmployeeListJson(View):
+    def get(self, request, *args, **kwargs):
+        # Извлечь всех работников из базы данных
+        employees = Employee.objects.all()
+
+        # Создаем json с полным пакетом информации на каждого работника
+        employees_json = [
+            {
+                "id": employee.id,
+                "name": employee.name,
+                "position": employee.position,
+                "email": employee.email,
+                "date_joined": employee.date_joined,
+                #"parent_name": [parentemployee.name for parentemployee in employees if
+                                #employee.parent_id == parentemployee.id]
             } for employee in employees
         ]
         return JsonResponse(employees_json, safe=False)
